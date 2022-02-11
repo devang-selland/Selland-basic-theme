311f2e62eb57cdb5e51a0e18f9e768a14640d120
devang-selland <devang.pujara@selland.technology>
2022-02-11 18:11:41 +0530

    ACF fields revision control

--- functions.php
+++ functions.php
@@ -35,6 +35,7 @@ $class_files = array(
 	'class-base-theme-acf-dynamic-gutenberg.php',
 	'class-base-theme-acf-theme-functions.php',
 	'class-base-theme-acf-missing.php',
+	'class-base-theme-acf-fields-sync.php',
 );
 foreach($class_files as $class_file){
     require_once 'classes/' . $class_file ;
